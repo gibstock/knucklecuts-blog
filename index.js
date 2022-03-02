61722321fa8b7d20d505f542400e11c3f919fa25
Andre Gonzales <gibstock@gmail.com>
2022-03-02 13:19:35 -0800

    adding tutorial section

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const axios = require('axios')
 const path = require('path');
 const marked = require('marked')
 const PORT = process.env.PORT || 5858
-const convert = require('./public/js/test')
+const convert = require('./public/js/test');
+const { response } = require('express');
 const URL = 'https://knuckle-cuts-db.herokuapp.com/api/blogs?populate=*'
+const URLTUTE = 'https://knuckle-cuts-db.herokuapp.com/api/tutorials?populate=*'
 
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
@@ -16,6 +18,18 @@ app.get('/', (req, res) => {
   axios.get(URL)
     .then(response => {
         res.render('pages/index', {
+          "data": response.data.data,
+          "port": PORT,
+          "convert": convert.formatDate,
+          "url":"https://knuckle-cuts-db.herokuapp.com"
+        })
+    })
+    .catch(err => console.error(err))
+})
+app.get('/tutorials/', (req, res) => {
+  axios.get(URLTUTE)
+    .then(response => {
+        res.render('pages/tutorials', {
           "data": response.data.data,
           "port": PORT,
           "convert": convert.formatDate,
@@ -63,4 +77,41 @@ app.get('/articles/:id', (req, res) => {
     .catch(err => console.error(err))
 })
   
+app.get('/tutorials/:id', (req,res) => {
+  axios.get(URLTUTE)
+    .then(response => {
+      response.data.data.map((item) => {
+        let unmarked = item.attributes.content;
+        let photoFormat
+        if((Object.keys(item.attributes.cover.data.attributes.formats)).includes("medium")) {
+          photoFormat = item.attributes.cover.data.attributes.formats.medium.url;
+        } else if((Object.keys(item.attributes.cover.data.attributes.formats)).includes("small")) {
+          photoFormat = item.attributes.cover.data.attributes.formats.small.url;
+        } else {
+          photoFormat = item.attributes.cover.data.attributes.formats.thumbnail.url;
+        }
+        if(req.params.id===item.attributes.slug) {
+          res.render('pages/blog-template', {
+            "title": item.attributes.title,
+            "author": item.attributes.author,
+            "content": marked.parse(unmarked),
+            "photo": photoFormat,
+            "port": PORT,
+            "url": 'https://knuckle-cuts-db.herokuapp.com',
+            "time": convert.formatDate(item.attributes.publishedAt),
+            "related": item.attributes.relatedPosts.intro,
+            "category": "Tutorial",
+            // SEO
+            "description": item.attributes.seo.metaDescription,
+            "keywords": item.attributes.keywords,
+            "shareImage": item.attributes.cover.data.attributes.formats.thumbnail.url,
+            "metaTitle": item.attributes.seo.metaTitle,
+            "preventIndexing": item.attributes.preventIndexing,
+            "slug": item.attributes.slug
+          })
+        }
+      })
+
+    })
+})
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`))
